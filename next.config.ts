1156669d658c72c3460dfda82de42a4db01ca5fe
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-05-13 23:46:35 +0600

    feat: Next.js config  pattern updated  for YT urls

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com', // YouTube Avatar domain
+      },
+      {
+        protocol: 'https',
+        hostname: 'yt3.googleusercontent.com', // Alternative Avatar domain
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com', // YouTube Video Thumbnail domain
+      }
+    ],
+  },
 };
 
 export default nextConfig;
